5d51826c3e332255b1fee7b4c838925c077f1362
JoaoPauloFialho <jpcarneirofialho@gmail.com>
2023-01-22 02:17:47 -0300

    mudança no estilo da página login

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -4,7 +4,7 @@ import { useUserContext } from '../../common/contexts/User'
 import styles from './Login.module.scss'
 import icone_olho from './olhoicone.png'
 
-export default function Login({redirect}){
+export default function Login({ redirect }) {
     const nav = useNavigate()
     const [email, setEmail] = useState()
     const [senha, setSenha] = useState()
@@ -29,42 +29,47 @@ export default function Login({redirect}){
         event.preventDefault()
         if (loginCorreto(email, senha)) {
             fazLogin(email, senha)
-            if(redirect) nav('/')
+            if (redirect) nav('/')
             alert('Usuário Logado')
         } else {
             let loginInvalido = <p className={styles.erro}>Usuário ou senha inválidos</p>
             setLoginInvalido(loginInvalido)
         }
     }
-    
-    return(
+
+    return (
         <section className={styles.login}>
-                <span className={styles.login__form}>
-                    <form onSubmit={(event) => aoLogar(event)}>
-                        <h1>Fazer Login</h1>
-                        <input
-                            onChange={(event) => aoMudarEmail(event)}
-                            name='usuario'
-                            type='text'
-                            placeholder='Usuario'
-                        />
-                        <input
-                            onChange={(event) => aoMudarSenha(event)}
-                            name='senha' type={tipoInput}
-                            placeholder='Senha'
-                        />
-                        {loginInvalido ?
-                            loginInvalido :
-                            ''
-                        }
-                        <img onClick={() => mostrarSenha()} src={icone_olho} alt='mostrar senha' />
-                        <button>Logar</button>
-                    </form>
-                </span>
-                <span className={styles.login__nao_possui}>
-                    <p>Não possui cadastro no site?</p>
-                    <Link to='/user/cadastrar'>Clique aqui e realize</Link>
-                </span>
-            </section>
+            <span className={styles.login__form}>
+                <form 
+                className={styles.login__form__formulario}
+                onSubmit={(event) => aoLogar(event)}
+                >
+                    <h1>Fazer Login</h1>
+                    <input
+                        className={styles.login__form__formulario__usuario}
+                        onChange={(event) => aoMudarEmail(event)}
+                        name='usuario'
+                        type='text'
+                        placeholder='Usuario'
+                    />
+                    <input
+                        className={styles.login__form__formulario__senha}
+                        onChange={(event) => aoMudarSenha(event)}
+                        name='senha' type={tipoInput}
+                        placeholder='Senha'
+                    />
+                    {loginInvalido ?
+                        loginInvalido :
+                        ''
+                    }
+                    <img onClick={() => mostrarSenha()} src={icone_olho} alt='mostrar senha' />
+                    <button>Logar</button>
+                </form>
+            </span>
+            <span className={styles.login__nao_possui}>
+                <p>Não possui cadastro no site?</p>
+                <Link to='/user/cadastrar'>Clique aqui e realize</Link>
+            </span>
+        </section>
     )
 }
